7a92dc28652609c1b91eac37f1793ec0dfdb1fb3
Marcap00 <cappellatomarco@yahoo.com>
2025-02-18 00:48:36 +0100

    Implement image picker functionality on home screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,10 +3,27 @@ import { Link } from 'expo-router';
 import { Image } from 'expo-image';
 import ImageViewer from '@/components/ImageViewer';
 import Button from '@/components/Button';
+import * as ImagePicker from 'expo-image-picker';
+
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+      mediaTypes: ['images'],
+    })
+
+    if (!result.canceled) {
+      console.log(result);
+      console.log(result.assets[0].uri);
+      return result.assets[0].uri;
+    } else {
+      alert("You need to provide a photo");
+    }
+  }
   return (
     <View style={styles.container}>
       {/* <Text style={styles.text}>Home screen</Text>
