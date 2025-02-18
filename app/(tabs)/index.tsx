680669d9f4a03d2f71e936940d6c77eaac496851
Marcap00 <cappellatomarco@yahoo.com>
2025-02-18 15:03:52 +0100

    Wrap main component with GestureHandlerRootView for gesture support

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -12,6 +12,7 @@ import EmojiPicker from '@/components/EmojiPicker';
 import { ImageSource } from 'expo-image';
 import EmojiList from '@/components/EmojiList';
 import EmojiSticker from '@/components/EmojiSticker';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
@@ -69,7 +70,7 @@ export default function Index() {
     }
 
     return (
-        <View style={styles.container}>
+        <GestureHandlerRootView style={styles.container}>
             <View style={styles.imageContainer}>
                 <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
                 {pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />}
@@ -92,7 +93,7 @@ export default function Index() {
             <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
                 <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
             </EmojiPicker>
-        </View>
+        </GestureHandlerRootView>
     );
 }
 
