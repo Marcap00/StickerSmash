a1be3b17df497ed58805842b3b6ca544d4dd5403
Marcap00 <cappellatomarco@yahoo.com>
2025-02-18 15:19:55 +0100

    Add double-tap scaling gesture to EmojiSticker component

--- components/EmojiSticker.tsx
+++ components/EmojiSticker.tsx
@@ -1,5 +1,7 @@
 import { View } from 'react-native';
 import { Image, type ImageSource } from 'expo-image';
+import Animated, { useSharedValue, useAnimatedStyle, withSpring } from 'react-native-reanimated';
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
 
 type Props = {
     imageSize: number;
@@ -7,9 +9,35 @@ type Props = {
 };
 
 export default function EmojiSticker({ imageSize, stickerSource }: Props) {
+    const scaleImage = useSharedValue(imageSize);
+
+    const doubleTap = Gesture.Tap()
+        .numberOfTaps(2)
+        .onStart(() => {
+            if (scaleImage.value !== imageSize * 2) { // if the image is not at the max size, double the size
+                scaleImage.value = scaleImage.value * 2;
+            } else { // if the image is at the max size, halve the size
+                scaleImage.value = Math.round(scaleImage.value / 2);
+            }
+        });
+
+    const imageStyle = useAnimatedStyle(() => {
+        return {
+            width: withSpring(scaleImage.value),
+            height: withSpring(scaleImage.value),
+        }
+    });
+
     return (
         <View style={{ top: -350 }}>
-            <Image source={stickerSource} style={{ width: imageSize, height: imageSize }} />
+            {/* <Image source={stickerSource} style={{ width: imageSize, height: imageSize }} /> */}
+            <GestureDetector gesture={doubleTap}>
+                <Animated.Image
+                    source={stickerSource}
+                    style={[imageStyle, { width: imageSize, height: imageSize }]}
+                    resizeMode="contain"
+                />
+            </GestureDetector>
         </View>
     );
 }
